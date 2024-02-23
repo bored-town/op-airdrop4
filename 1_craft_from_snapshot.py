be7121256f2eb5e39d4868971e134739d7f34c97
diewland <diewland@gmail.com>
2024-02-24 00:44:01 +0700

    revise lowercase address, add no

--- 1_craft_from_snapshot.py
+++ 1_craft_from_snapshot.py
@@ -5,58 +5,71 @@ from web3.utils.address import to_checksum_address
 
 chunk = {}
 
-# load snapshot data
+# 1) load snapshot data
 for (title, fname, score) in CONFIG_COL:
     # read file line by line
     src_path = '{}/{}'.format(DIR_SNAPSHOT, fname)
     for line in open(src_path, 'r'):
         (addr, qty) = line.strip().split(',')
-        addr = addr.lower() # [!] store address in lowercase format
-                            # [!] prevent duplicate sensitive address
+        addr = addr.lower() # [!] use address in lowercase format
+                            # [!] prevent sensitive address issue
         # add score to wallet
         info = chunk.get(addr)
         if info is None:
             chunk[addr] = {}
         chunk[addr][title] = score
 
-# recheck duplicate address
+# 2) recheck duplicate address
 dup_addrs = find_duplicates_ignore_case(chunk.keys())
 if len(dup_addrs) > 0:
     print(dup_addrs)
     print('found {} duplicated addresses'.format(len(dup_addrs)))
     exit()
 
-# remove ban list
+# 3) remove ban list
 for addr in BAN_WALLETS:
     del chunk[addr.lower()]
 
-# sum points
+# 4) sum points
 total_points = 0
 for (addr, info) in chunk.items():
-    info['sum'] = sum(info.values())
-    total_points += info['sum']
+    info['points'] = sum(info.values())
+    total_points += info['points']
 
-# calc OP
+# 5) before reshape
 for (addr, info) in chunk.items():
-    info['op'] = REWARD_OP * (info['sum'] / total_points)
+    # add checksum address
+    info['addr'] = to_checksum_address(addr)
+    # calc OP
+    info['op'] = REWARD_OP * (info['points'] / total_points)
 
-# reshape output
-output = []
-for (addr, info) in chunk.items():
-    body = [ to_checksum_address(addr) ]
-    body.append(info['op'])
-    body.append(info['sum'])
-    for (title, _, _) in CONFIG_COL:
-        body.append(info.get(title) or 0)
-    output.append(body)
+# 6) reshape chunk to list of dict
+chunk = chunk.values()
 
-# sort by points
-output = sorted(output, key=lambda x: -x[2])
+# 7) sort by points, address
+chunk = sorted(chunk, key=lambda x: (-x['points'], x['addr']))
 
-# print output (header)
+# 8) add no
+cur_no = None
+cur_points = None
+for (idx, info) in enumerate(chunk):
+    if info['points'] != cur_points:
+        cur_no = idx + 1
+        cur_points = info['points']
+    info['no'] = cur_no
+
+# 9) print output (header)
 fields = ','.join([ title for (title, _, _) in CONFIG_COL ])
-print("address,OP,sum,{}".format(fields))
+print("no,address,OP,points,{}".format(fields))
 
-# print output (body)
-for row in output:
-    print(','.join([ str(r) for r in row ]))
+# 10) print output (body)
+for c in chunk:
+    op     = math.floor(c['op'] * 1_000) / 1_000 # floor 3 digits
+    fields = ','.join([ str(c.get(title) or 0) for (title, _, _) in CONFIG_COL ])
+    print('{},{},{},{},{}'.format(
+        c['no'],        # no
+        c['addr'],      # addr
+        op,             # OP reward
+        c['points'],    # points
+        fields,         # collection points
+    ))
