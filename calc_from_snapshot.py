2cb4c80271df0516e238826e781f66b8ab954e22
diewland <diewland@gmail.com>
2024-02-24 12:14:40 +0700

    do not show OP yet

--- calc_from_snapshot.py
+++ calc_from_snapshot.py
@@ -1,8 +1,8 @@
+import math
+from web3.utils.address import to_checksum_address
+from pprint import pprint as pp
 from config import *
 from common import *
-from pprint import pprint as pp
-import os, time, math
-from web3.utils.address import to_checksum_address
 
 chunk = {}
 
@@ -61,11 +61,11 @@ for (idx, info) in enumerate(chunk):
 
 # 9) print output (header)
 fields = ','.join([ title for (title, _, _) in CONFIG_COL ])
-print("no,address,OP,points,{}".format(fields))
+print("#,Address,OP,Points,{}".format(fields))
 
 # 10) print output (body)
 for c in chunk:
-    op     = math.floor(c['op'] * 1_000) / 1_000 # floor 3 digits
+    op = 'TBA' #math.floor(c['op'] * 1_000) / 1_000 # floor 3 digits
     fields = ','.join([ str(c.get(title) or 0) for (title, _, _) in CONFIG_COL ])
     print('{},{},{},{},{}'.format(
         c['no'],        # no
